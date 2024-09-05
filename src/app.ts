47612448e318fbd140387285753fdbea13b823a5
Vinícius Eduardo <viniciuseduardocorreia@hotmail.com>
2024-09-05 11:35:28 -0300

    projeto

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import { DatabaseModel } from './model/DatabaseModel';
+
+//definindo a porta do servidor
+const port: number = 3333;
+
+//Criando servidor web
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+//rotas da aplicação
+server.get('/', (req, res) => {
+    res.json({ mensagem: "Olá mundo, esta é minha primeira aplicação web"});
+});
+
+server.get('/pessoas', (req, res) => {
+    res.json({ mensagem: "Aqui será apresentado os dados das pessoas"});
+});
+
+new DatabaseModel().testeConexao().then((resdb) => {
+    if(resdb){
+        console.log("Conexão com banco de dados realizada com sucesso!");
+        // Iniciando o servidor
+        server.listen(port, () => {
+            console.log(`Servidor iniciado no endereço http://localhost:${port}`);
+        });
+    } else {
+        console.log("Erro ao conectar ao banco de dados!");
+    }
+});
+
+
